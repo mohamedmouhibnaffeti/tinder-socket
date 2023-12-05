0cf243f8298757c1cdee0c455be995dcf6a811e1
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2023-12-05 22:26:28 +0100

    fixed chat errors

--- index.js
+++ index.js
@@ -2,25 +2,41 @@ const io = require('socket.io')(8800, {
     cors: {
         origin: "http://localhost:3000"
     }
-})
+});
 
-let activeUsers = []
+// Use a shared variable accessible across different socket events
+let activeUsers = [];
 
-io.on("connection", (socket)=>{
-    //add new user
-    socket.on('new-user-add', (newUserEmail)=>{
-        if(!activeUsers.some((user)=>user.user.email === newUserEmail)){
+io.on("connection", (socket) => {
+    // add new user
+    socket.on('new-user-add', (newUserEmail) => {
+        if (!activeUsers.some((user) => user.email === newUserEmail)) {
             activeUsers.push({
-                userEmail: newUserEmail,
+                email: newUserEmail,
                 socketId: socket.id
-            })
+            });
         }
-        console.log('Connected users :>> ', activeUsers);
-        io.emit('get-users', activeUsers)
-    })
-    socket.on('disconnect', ()=>{
-        activeUsers = activeUsers.filter((user)=>user.socketId !== socket.id)
-        console.log("user disconnected...", activeUsers)
-        io.emit('get-users', activeUsers)
-    })
-})
+        io.emit('get-users', activeUsers);
+    });
+
+    socket.on('disconnect', () => {
+        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+        console.log("user disconnected...", activeUsers);
+        io.emit('get-users', activeUsers);
+    });
+
+    socket.on('send-message', (data) => {
+        const receiverEmail = data.receiverEmail;
+        const sender = data.sender
+        const user = activeUsers.find((user) => user.email === receiverEmail);
+        const senderUser = activeUsers.find((user) => user.email === sender);
+        if(senderUser){
+            io.to(senderUser.socketId).emit("receive-message", data);
+        }
+        console.log(data);
+        if (user) {
+            io.to(user.socketId).emit("receive-message", data);
+            console.log(activeUsers);
+        }
+    });
+});
